442bca59350be186f0b44d8639f7b11c7b40b475
Oleg <89264247+GeloPakDev@users.noreply.github.com>
2024-01-15 23:23:51 +0500

    feat(registration): Implement email based sign up (#4)
    * feat(registration): Implement email based sign up
    - Add service class with the registration logic connected with firebase
    - Implement of saving user in the database while user sign up
    - Add controllers into the Registration Screen
    - Initialize the Firebase app in the main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled/pages/registration_screen.dart';
 import 'pages/welcome_screen.dart';
@@ -5,7 +8,16 @@ import 'pages/login_screen.dart';
 import 'pages/navigation_screen.dart';
 import 'pages/pets_list_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'AIzaSyCXWx205p0yW9hpMmxs0uOTZdSOCh_n698',
+              appId: '1:244911606193:android:8cb01e01d4a3e7320a5590',
+              messagingSenderId: '244911606193',
+              projectId: 'petadoption-158ed'))
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
