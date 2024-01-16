4deba2801bbe3cdd58af6e57b328f9ae8c5c01df
sabrinaaa17 <2110166@newuzbekistanuniversity.uz>
2024-01-17 00:48:54 +0500

    feat: new pages were added, navigation to new pages was added

---
+++ lib/pages/adoption/adoption_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AdoptionPage extends StatefulWidget {
+  const AdoptionPage({super.key});
+
+  @override
+  State<AdoptionPage> createState() => _AdoptionPageState();
+}
+
+class _AdoptionPageState extends State<AdoptionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
---
+++ lib/pages/recommendations_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RecommendationsPage extends StatefulWidget {
+  const RecommendationsPage({super.key});
+
+  @override
+  State<RecommendationsPage> createState() => _RecommendationsPageState();
+}
+
+class _RecommendationsPageState extends State<RecommendationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
