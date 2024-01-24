7aca8146cf4824da6da4e31ee33c59070e8a5365
sabrinaaa17 <2110166@newuzbekistanuniversity.uz>
2024-01-25 00:44:35 +0500

    fix: add_pet_page and pet_card_widged fixed ui problems

--- lib/pages/adoption/add_pet_page.dart
+++ lib/pages/adoption/add_pet_page.dart
@@ -6,6 +6,7 @@ class Pet {
   int age;
   String breed;
   String gender;
+  String petType;
   String petPhoto;
   Map<String, String> medicalInfo;
 
@@ -14,6 +15,7 @@ class Pet {
     required this.age,
     required this.breed,
     required this.gender,
+    required this.petType,
     required this.petPhoto,
     required this.medicalInfo,
   });
@@ -31,14 +33,15 @@ class _AdoptionInputPageState extends State<AdoptionInputPage> {
   TextEditingController medicalInfoController = TextEditingController();
   TextEditingController additionalInfoController = TextEditingController();
 
-  String petType = '';
   String gender = '';
+  String petType = '';
 
   Pet pet = Pet(
     name: '',
     age: 0,
     breed: '',
     gender: '',
+    petType: '',
     petPhoto: '',
     medicalInfo: {},
   );
@@ -49,140 +52,174 @@ class _AdoptionInputPageState extends State<AdoptionInputPage> {
       appBar: AppBar(
         title: Text('Pet Adoption'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Circular image placeholder
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(width: 2, color: Colors.grey),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(pet.petPhoto),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Circular image placeholder
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 2, color: Colors.grey),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(pet.petPhoto),
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 16),
+              SizedBox(height: 16),
 
-            // Input field for pet name
-            InputWidget(
-              label: 'Name',
-              controller: nameController,
-            ),
+              // Input field for pet name below the image
+              InputWidget(
+                label: 'Pet Name',
+                controller: nameController,
+              ),
 
-            SizedBox(height: 16),
+              SizedBox(height: 16),
 
-            // Input field for pet age
-            InputWidget(
-              label: 'Age',
-              controller: ageController,
-            ),
-
-            SizedBox(height: 16),
-
-            // Selection field for pet type
-            InputWidget(
-              label: 'Pet Type',
-              controller: TextEditingController(text: petType),
-              readOnly: true,
-              onTap: () {
-                // Implement logic for selecting pet type
-              },
-            ),
-
-            SizedBox(height: 16),
-
-            // Input field for breed
-            InputWidget(
-              label: 'Breed',
-              controller: breedController,
-            ),
-
-            SizedBox(height: 16),
-
-            // Selection field for gender
-            InputWidget(
-              label: 'Gender',
-              controller: TextEditingController(text: gender),
-              readOnly: true,
-              onTap: () {
-                // Implement logic for selecting gender
-              },
-            ),
-
-            SizedBox(height: 16),
-
-            // Input field for medical information
-            InputWidget(
-              label: 'Medical Info',
-              controller: medicalInfoController,
-            ),
-
-            SizedBox(height: 16),
-
-            // Input field for additional information
-            InputWidget(
-              label: 'Additional Info (Optional)',
-              controller: additionalInfoController,
-            ),
-
-            SizedBox(height: 16),
-
-            // Buttons
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement logic for cancel button
-                    Navigator.pop(context);
-                  },
-                  child: Text('Cancel'),
+              // Space for displaying the pet's name
+              Text(
+                pet.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
                 ),
-                SizedBox(width: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement logic for add pet button
+              ),
 
-                    // Input validation
-                    if (nameController.text.isEmpty ||
-                        ageController.text.isEmpty ||
-                        breedController.text.isEmpty ||
-                        medicalInfoController.text.isEmpty ||
-                        petType.isEmpty ||
-                        gender.isEmpty) {
-                      // Display an error message or handle it as appropriate
-                      print('Please fill in all required fields.');
-                      return;
-                    }
+              SizedBox(height: 16),
 
-                    // Save data to the Pet object
-                    pet = Pet(
-                      name: nameController.text,
-                      age: int.parse(ageController.text),
-                      breed: breedController.text,
-                      gender: gender,
-                      petPhoto: '', // Replace with actual image URL
-                      medicalInfo: {
-                        'info': medicalInfoController.text,
-                      },
-                    );
+              // Input field for pet age
+              InputWidget(
+                label: 'Age',
+                controller: ageController,
+              ),
 
-                    // Do something with the pet data (e.g., save to database)
-                    // For now, just print the pet details
-                    //print('Pet Details: ${pet.name}, ${pet.age}, ${pet.breed}, ${pet.gender}, ${pet.petPhoto}, ${pet.medicalInfo}');
-                    Navigator.pushNamed(context, '/my_pets_page');
-                  },
-                  child: Text('Add Pet'),
+              SizedBox(height: 16),
+
+              // Dropdown field for pet type
+              DropdownButtonFormField<String>(
+                decoration: InputDecoration(
+                  labelText: 'Pet Type',
+                  border: OutlineInputBorder(),
                 ),
-              ],
-            ),
-          ],
+                value: petType,
+                items: ['Dog', 'Cat', 'Bird', 'Others'].map((String type) {
+                  return DropdownMenuItem<String>(
+                    value: type,
+                    child: Text(type),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    petType = value ?? '';
+                  });
+                },
+              ),
+
+              SizedBox(height: 16),
+
+              // Input field for breed
+              InputWidget(
+                label: 'Breed',
+                controller: breedController,
+              ),
+
+              SizedBox(height: 16),
+
+              // Dropdown field for gender
+              DropdownButtonFormField<String>(
+                decoration: InputDecoration(
+                  labelText: 'Gender',
+                  border: OutlineInputBorder(),
+                ),
+                value: gender,
+                items: ['Male', 'Female'].map((String gen) {
+                  return DropdownMenuItem<String>(
+                    value: gen,
+                    child: Text(gen),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    gender = value ?? '';
+                  });
+                },
+              ),
+
+              SizedBox(height: 16),
+
+              // Input field for medical information
+              InputWidget(
+                label: 'Medical Info',
+                controller: medicalInfoController,
+              ),
+
+              SizedBox(height: 16),
+
+              // Input field for additional information
+              InputWidget(
+                label: 'Additional Info (Optional)',
+                controller: additionalInfoController,
+              ),
+
+              SizedBox(height: 16),
+
+              // Buttons
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement logic for cancel button
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cancel'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement logic for add pet button
+
+                      // Input validation
+                      if (nameController.text.isEmpty ||
+                          ageController.text.isEmpty ||
+                          breedController.text.isEmpty ||
+                          medicalInfoController.text.isEmpty ||
+                          petType.isEmpty ||
+                          gender.isEmpty) {
+                        // Display an error message or handle it as appropriate
+                        print('Please fill in all required fields.');
+                        return;
+                      }
+
+                      // Save data to the Pet object
+                      pet = Pet(
+                        name: nameController.text,
+                        age: int.parse(ageController.text),
+                        breed: breedController.text,
+                        gender: gender,
+                        petType: petType,
+                        petPhoto: '', // Replace with actual image URL
+                        medicalInfo: {
+                          'info': medicalInfoController.text,
+                        },
+                      );
+
+                      // Do something with the pet data (e.g., save to database)
+                      // For now, just print the pet details
+                      //print('Pet Details: ${pet.name}, ${pet.age}, ${pet.breed}, ${pet.gender}, ${pet.petType}, ${pet.petPhoto}, ${pet.medicalInfo}');
+                      Navigator.pushNamed(context, '/my_pets_page');
+                    },
+                    child: Text('Add Pet'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
